2da124445119f6a9c71f583a5fb836f4c489dbb5
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-10 00:31:04 +0600

    Adding my first project_Hello World

---
+++ FirstProgram-Solution/FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello,Dev Skill!!!!";
+            string y = "Its a training Academy....";
+            Console.WriteLine(x + y);
+        }
+    }
+}
