4130b49e18d5bb08f2b15000c4da07d654f612e2
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-10 00:47:25 +0600

    Simple addtion program in csharp code

---
+++ SimpleAddition-Solution/SimpleAddition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10 , y = 15;
+            int sum = x + y;
+
+
+            Console.WriteLine("Summation of " + x + " and " + y + " is = " + sum);
+        }
+    }
+}
