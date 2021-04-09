f8158f3933cba9bea34155253aa135c0d729c352
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-10 00:43:44 +0600

    My introduction in Csharp code

---
+++ Introduction-Solution/Introduction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Angon Chowdhury";
+            Console.WriteLine("Hello people");
+            Console.WriteLine("This is " + name);
+
+            string address = "Chittagong";
+            Console.WriteLine("I'm from " + address + ", Bangladesh!!!!");
+        }
+    }
+}
