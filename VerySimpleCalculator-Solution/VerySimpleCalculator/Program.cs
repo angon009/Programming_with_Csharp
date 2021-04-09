d223c75a4c5470bfb0fb5a7925e10af09c89c1ee
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-10 01:43:07 +0600

    A very simple calculator which does addition,subtraction,multiplication,division and modulus operation

---
+++ VerySimpleCalculator-Solution/VerySimpleCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VerySimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10 , y = 5;
+            int sum = x + y;
+            int sub = x - y;
+            int mul = x * y;
+            int div = x / y;
+            int mod = x % y;
+            Console.WriteLine("Result of Addition = " + sum);
+            Console.WriteLine("Result of Subtraction = " + sub);
+            Console.WriteLine("Result of Multiplication = " + mul);
+            Console.WriteLine("Result of Division = " + div);
+            Console.WriteLine("Result of Modulus = " + mod);
+             
+        }
+    }
+}
