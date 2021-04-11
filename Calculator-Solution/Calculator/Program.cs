806ff4ccbf0cb5c6038a3ac311d0bdbc706646cc
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-11 23:07:53 +0600

    Simple calculator by taking input from user

---
+++ Calculator-Solution/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNum, secondNum ;
+            firstNum = Console.ReadLine();
+            secondNum = Console.ReadLine();
+             
+
+            int first = Convert.ToInt32(firstNum);
+            int second = Convert.ToInt32(secondNum);
+             
+
+            int add = first + second;
+            int sub = first - second;
+            int mul = first * second;
+            double div = first / second;
+            int mod = first % second;
+
+            Console.WriteLine("Result of addition = " + add);
+            Console.WriteLine("Result of subtraction = " + sub);
+            Console.WriteLine("Result of Multiplication = " + mul);
+            Console.WriteLine("Result of Division = " + div);
+            Console.WriteLine("Result of Modulas = " + mod);
+
+            
+        }
+    }
+}
