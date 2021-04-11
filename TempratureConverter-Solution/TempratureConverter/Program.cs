ffaef782327de579dc299bab02afc8d13d4beb8f
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-11 22:43:30 +0600

    Simple Temperature Converter (Cel to Fah)

---
+++ TempratureConverter-Solution/TempratureConverter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+ 
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cel = 30;
+            double fah;
+            fah = (160 + 9 * cel) / 5;
+            Console.WriteLine(cel + " degree celcius = " + fah + " degree Fahrenhet");
+        }
+    }
+ 
