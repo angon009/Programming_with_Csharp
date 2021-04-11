dc21f6de1583da2cfddb5e25b599159bca4129ff
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-11 22:54:31 +0600

    Largest among three numbers

---
+++ largestNumber-Solution/largestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 3;
+            int secondNum = 2;
+            int thirdNum = 4;
+            
+            if(firstNum >= secondNum && firstNum >= thirdNum){
+            Console.WriteLine(firstNum + " is largest!!!");
+        }
+            else if (secondNum >= firstNum && secondNum >= thirdNum) {
+            Console.WriteLine(secondNum + " is largest!!!");
+        }
+            else{
+            Console.WriteLine(thirdNum + " is largest!!!!");
+        }
+        }
+    }
+ 
