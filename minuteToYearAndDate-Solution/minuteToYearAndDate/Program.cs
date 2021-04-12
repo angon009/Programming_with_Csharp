1fb2772bdcb7fac67425f66a5efa91532f8da88f
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-13 01:33:10 +0600

    Minutes to Years and Days

---
+++ minuteToYearAndDate-Solution/minuteToYearAndDate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace minuteToYearAndDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Total Minutes = ");
+            string minsTemp = Console.ReadLine();
+
+            int mins = Convert.ToInt32(minsTemp);
+
+            int years = mins / 525600;
+
+            int remMins = mins - (years * 525600);
+
+            int days = remMins / 1440;
+
+            Console.WriteLine(mins + " minutes = " + years + " years and " + days + " days");
+
+
+        }
+    }
+}
