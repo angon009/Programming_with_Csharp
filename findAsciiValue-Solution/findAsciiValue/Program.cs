5dc0fb269080406286c128ab003744720d6527bb
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-13 01:18:05 +0600

    ASCII Value Finder

---
+++ findAsciiValue-Solution/findAsciiValue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace findAsciiValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character : ");
+            char inputChar = Console.ReadLine()[0];
+
+            int asciiVal = inputChar;
+
+            Console.WriteLine("ASCII value of " + asciiVal + " is : " +asciiVal);
+
+        }
+    }
+}
