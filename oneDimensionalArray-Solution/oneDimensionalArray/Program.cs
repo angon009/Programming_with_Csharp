7ef6a42a30a0119a97cb6d3bff979b9aff30534d
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-13 01:43:41 +0600

    Simple One Dimensional Array I/O

---
+++ oneDimensionalArray-Solution/oneDimensionalArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace oneDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One Dimensional Array");
+
+            Console.Write("Enter the size of array = ");
+
+            string temp = Console.ReadLine();
+
+            int size = Convert.ToInt32(temp);
+
+            int[] firstArr = new int[size];
+
+            Console.WriteLine("Enter the values = ");
+
+            for(int i = 0; i < size; i++)
+            {
+                string temp2 = Console.ReadLine();
+                int value = Convert.ToInt32(temp2);
+                firstArr[i] = value;
+            }
+
+            Console.WriteLine("Created Array : ");
+
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write(firstArr[i] + "    ");
+            }
+        }
+    }
+}
