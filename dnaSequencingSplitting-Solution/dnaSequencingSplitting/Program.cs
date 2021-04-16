d40262c15750b0da5c38f80846bd3b77363e511b
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-17 00:28:53 +0600

    String Manipulation Usage of split function

---
+++ dnaSequencingSplitting-Solution/dnaSequencingSplitting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dnaSequencingSplitting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DNA Sequence Splitting");
+
+            Console.WriteLine("Enter Sequence : ");
+            var sequence = Console.ReadLine();
+
+            var result = sequence.Split(':');
+
+            var nucleotidePart = int.Parse(result[0]);
+            var nucleosidePart = double.Parse(result[1]);
+            var satChrormosome = decimal.Parse(result[2]);
+            var isXYChromsomePresent = bool.Parse(result[3]);
+
+            Console.WriteLine(nucleotidePart);
+            Console.WriteLine(nucleosidePart);
+            Console.WriteLine(satChrormosome);
+            Console.WriteLine(isXYChromsomePresent);
+
+
+
+        }
+    }
+}
