acb76435c69a43eac4bf98ce212134cb90d1b707
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-17 00:26:40 +0600

    Sum of All Elements of An Array

---
+++ arraySum-Solution/arraySum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace arraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of all Elements of an Array : ");
+            Console.Write("How many Elements = ");
+            string totalTemp = Console.ReadLine();
+            int totalElement = Convert.ToInt32(totalTemp);
+
+            int[] arr = new int[totalElement];
+
+            Console.WriteLine("Enter the elements : ");
+            for(int i = 0; i < totalElement; i++)
+            {
+                string elementTemp = Console.ReadLine();
+                int element = Convert.ToInt32(elementTemp);
+                arr[i] = element;
+            }
+            int sum = 0;
+            for(int i = 0; i < totalElement; i++)
+            {
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine("Sum of all Elements = " + sum);
+        }
+    }
+}
