04bd0621ad51f52f2a51c8d89ea5adbe47a922c7
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-21 02:24:37 +0600

    Loop Implementation Examples

---
+++ Loops-Solution/basicWhileLoopExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace basicWhileLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decimal number to binary number by using while loop
+
+            Console.Write("Enter Decimal Number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            int[] binaryNumber = new int[1];
+
+
+            int result, i = 0;
+
+            while (number != 0 && i <= 7)
+            {
+                result = number % 2;
+                binaryNumber[i] = result;
+                number = number / 2;
+                i++;
+            }
+
+            for(var j = 7; j >= 0; j--)
+            {
+                Console.Write(binaryNumber[j]);
+            }
+
+        }
+    }
+}
+
