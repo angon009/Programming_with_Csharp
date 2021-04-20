39064da37af2102f33e2653972098c34c705c44b
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-04-21 01:42:55 +0600

    Updated

---
+++ Loops-Solution/Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Simple For Loop Implementation
+            //Two Series : One is incrementing in ascending order and Another one is incrementing in descending order
+            //Calculate sum of same index elements and print it
+
+            Console.Write("First value of ascending series = ");
+            int firstAscOrder = int.Parse(Console.ReadLine());
+
+            Console.Write("Last value of descending series =  ");
+            int lastAscOrder = int.Parse(Console.ReadLine());
+
+            Console.Write("First value of descending series = ");
+            int firstDescOrder = int.Parse(Console.ReadLine());
+
+            Console.Write("Last value of descending series = ");
+            int lastDescOrder = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for(int i = firstAscOrder , j = firstDescOrder; i <= lastAscOrder && j >= lastDescOrder; i++, j--)
+            {
+                sum = i + j;
+                Console.WriteLine($"{i} + {j} = {sum}");
+            }
+            Console.WriteLine("Successfully Compiled!!!!");
+        }
+    }
+}
---
+++ testConsole-Solution/testConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lineNumber = Console.ReadLine();
+            var individualNumber = lineNumber.Split(',');
+
+            var firstPart = int.Parse(individualNumber[0]);
+            var secondPart = double.Parse(individualNumber[1]);
+            var thirdPart = int.Parse(individualNumber[2]);
+
+             
+            Console.WriteLine(firstPart);
+            Console.WriteLine(secondPart);
+            Console.WriteLine(thirdPart);
+
+
+             
+        }
+    }
+}
