5372bafe5cf0300568821f1f7aeb9588c26703e4
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-05-18 20:35:52 +0600

    Date time modified

---
+++ DateTime-Solution/DateTimeFormatting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DateTimeFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime.ToString("dd MMMM, yyyy"));
+            Console.WriteLine(dateTime.ToString("MMMM dd, yyyy"));
+            Console.WriteLine(dateTime.ToString("yyyy , MMMM dd"));
+            Console.WriteLine(dateTime.ToString("yyyy , dd MMMM"));
+
+
+
+            Console.WriteLine(dateTime.ToString("d"));
+            Console.WriteLine(dateTime.ToString("D"));
+
+            Console.WriteLine(dateTime.ToString("t"));
+            Console.WriteLine(dateTime.ToString("T"));
+
+            Console.WriteLine(dateTime.ToString("g"));
+            Console.WriteLine(dateTime.ToString("G"));
+
+            Console.WriteLine(dateTime.ToString("Y"));
+
+            Console.WriteLine(dateTime.ToString("dddd"));
+            Console.WriteLine(dateTime.ToString("MMMM"));
+            Console.WriteLine(dateTime.ToString("yyyy"));
+
+        }
+    }
+}
---
+++ DateTime-Solution/TimeZoneExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimeZoneExample
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            //UTC to Local
+            DateTime timeNowUtc1 = DateTime.UtcNow;
+            DateTime timeNowLocal1 = TimeZone.CurrentTimeZone.ToLocalTime(timeNowUtc1);
+            Console.WriteLine(timeNowLocal1);
+
+
+            //Local To UTC
+            DateTime timeNowLocal2 = DateTime.Now;
+            DateTime timeNowUtc2 = TimeZone.CurrentTimeZone.ToUniversalTime(timeNowLocal2);
+            Console.WriteLine(timeNowUtc2);
+
+            //All Time Zones
+            foreach(TimeZoneInfo i in TimeZoneInfo.GetSystemTimeZones())
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
