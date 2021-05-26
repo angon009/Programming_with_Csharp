953ae2da50bf378299c620be4f2dbaa264572c96
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-05-27 02:19:38 +0600

    Anonymous type modified

---
+++ AnonymousType-Solution/AnonymousArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace AnonymousArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentList = new[]
+            {
+                new{name = "John" , age = 24, id = "C181" , mail = "john@gmail.com"},
+                new{name = "Anton" , age = 22, id = "C182" , mail = "anton@gmail.com"},
+                new{name = "Danik", age = 21, id = "C183" , mail = "danik@gmail.com"},
+                new{name = "Alan", age = 27, id = "C182" , mail = "alan@gmail.com"}
+            };
+
+            for(int i = 0; i < studentList.Length; i++)
+            {
+                Console.WriteLine($"{studentList[i].name}  {studentList[i].age}  {studentList[i].id}  {studentList[i].mail}");
+            }
+
+            var searchFilter = studentList.Where(c => c.name.StartsWith("A")).ToList();
+
+            Console.WriteLine(searchFilter);
+
+            var studentName = studentList.FirstOrDefault(c => c.id == "C182");
+        }
+    }
+}
--- AnonymousType-Solution/AnonymousType/Program.cs
+++ AnonymousType-Solution/AnonymousType/Program.cs
@@ -6,12 +6,27 @@ namespace AnonymousType
     {
         static void Main(string[] args)
         {
-            dynamic obj = new { message = "Hello" , number = 10 };
-            var msg = obj.message = "Hi";
+            #region Example - 01
+            //dynamic obj = new { message = "Hello" , number = 10 };
+            //var msg = obj.message = "Hi";
 
-            obj.number = 11;
+            //obj.number = 11;
 
-            Console.WriteLine(msg);
+            //Console.WriteLine(msg);
+            #endregion
+
+            #region Example - 02
+
+            var studentInfo = new
+            {
+                name = "Angon",
+                age = 24,
+                weight = 64.7,
+                type = 'S'
+            };
+
+            Console.WriteLine(studentInfo);
+            #endregion
         }
     }
 }
