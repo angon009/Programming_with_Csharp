0fac56df016ad5b76a2211731743deff28de86aa
Angon Chowdhury <angonchowdhuryiiuc@gmail.com>
2021-08-22 02:11:25 +0600

    test cl

--- testConsole-Solution/testConsole/Program.cs
+++ testConsole-Solution/testConsole/Program.cs
@@ -3,25 +3,43 @@
 namespace testConsole
 {
     class Program
-    {         
-        static void Main(string[] args)
+    {
+		static double func(double x)
+		{
+			return x * x * x - 9 * x + 1;
+		}
+
+
+		static void bisection(double a, double b)
+		{
+			if (func(a) * func(b) >= 0)
+			{
+                Console.WriteLine("You have not assumed right a and b");
+				return;
+			}
+
+			double c = a;
+			while ((b - a) >= 0.0005)
+			{
+
+				c = (a + b) / 2;
+
+
+				if (func(c) == 0.0)
+					break;
+
+
+				else if (func(c) * func(a) < 0)
+					b = c;
+				else
+					a = c;
+			}
+            Console.WriteLine("The value of root is : " + c);
+		}
+		static void Main(string[] args)
         {
-            int total = int.Parse(Console.ReadLine());
-            int[] arr = new int[total];
-
-            string[] inputs = Console.ReadLine().Split(" ");
-            for(int i = 0; i < total; i++)
-            {
-                arr[i] = int.Parse(inputs[i]);
-            }
-
-            int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                sum = sum + arr[i];
-            }
-
-            Console.WriteLine(sum);
-        }
+			double a = 2, b = 4;
+			bisection(a, b);
+		}
     }
 }
